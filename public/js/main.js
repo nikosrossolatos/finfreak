24864a177dca2befdb77b517123726b38b7abe1e
Nick Rossolatos <spidey_nr@hotmail.com>
2017-03-19 13:55:30 +0200

    add bucket page icons and fix

--- public/js/main.js
+++ public/js/main.js
@@ -9,7 +9,7 @@
 
   showBucketButton.addEventListener("click", function(e) {
     e.preventDefault
-    togglePages()
+    renderBucketPage()
   })
 
   getProducts()
@@ -86,6 +86,12 @@
         }
       })
     })
+  }
+
+  function renderBucketPage() {
+    var bucketPage = document.getElementById("bucket-page")
+
+    togglePages()
   }
 
 
